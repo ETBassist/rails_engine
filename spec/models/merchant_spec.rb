70ae297d83619ce0cdab21842c31ec1ef5333dba
Eugene Theriault <hybridbassist@gmail.com>
2020-12-11 09:19:30 -0800

    Add test for Merchant->Invoice->Customer relation

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many :items }
+    it { should have_many :invoices }
+    it { should have_many(:customers).through(:invoices) }
   end
 end
