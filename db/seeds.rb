f5dd32c03d24ecd6b9fa5b87a973584f8a91c05a
Eugene Theriault <hybridbassist@gmail.com>
2020-12-11 09:39:31 -0800

    Update Item and db/seeds.rb to clear previous db
    Adds a dependent: :destroy to Item/invoice_items so that db will be
    cleared out if database is reseeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,13 @@ ActiveRecord::Base.connection.tables.each do |table|
   ActiveRecord::Base.connection.reset_pk_sequence!(table)
 end
 
+Item.destroy_all
+
 cmd = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails_engine_development db/data/rails-engine-development.pgdump"
 puts "Loading PostgreSQL Data dump into local database with command:"
 puts cmd
 system(cmd)
+
 
 CSV.foreach('./db/data/items.csv', headers: true) do |row|
   Item.create!(id: row["id"].to_i,
