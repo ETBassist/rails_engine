11a2acbef535353461fcfa10a1de697bf75217e7
Eugene Theriault <hybridbassist@gmail.com>
2020-12-11 09:05:52 -0800

    Create belongs_to tests for Invoice
    Should belong_to both Customer and Merchant models

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe Invoice, type: :model do
   describe 'relationships' do
+    it { should belong_to :customer }
+    it { should belong_to :merchant }
     it { should have_many :invoice_items }
     it { should have_many(:items).through(:invoice_items) }
   end
