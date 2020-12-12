684e41801307f05dafe754f59ee2292e8d5ee78c
Eugene Theriault <hybridbassist@gmail.com>
2020-12-12 11:22:08 -0800

    Create merchant POST endpoint

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,4 +8,15 @@ class Api::V1::MerchantsController < ApplicationController
     merchant = Merchant.find(params[:id])
     render json: MerchantSerializer.format_merchant(merchant)
   end
+
+  def create
+    merchant = Merchant.create!(merchant_params)
+    render json: MerchantSerializer.format_merchant(merchant)
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:name)
+  end
 end
