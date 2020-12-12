3ef6e12153d65290cfddc6d9aa489a137f8db7b0
Eugene Theriault <hybridbassist@gmail.com>
2020-12-11 18:58:48 -0800

    Create endpoint for Items Index

---
+++ app/serializers/items_serializer.rb
@@ -0,0 +1,19 @@
+class ItemsSerializer
+  def self.format_items(items)
+    {
+      data: items.map do |item|
+        {
+          id: item.id,
+          type: 'item',
+          attributes: {
+            name: item.name,
+            description: item.description,
+            unit_price: item.unit_price,
+            merchant_id: item.merchant_id
+
+          }
+        }
+      end
+    }
+  end
+end
