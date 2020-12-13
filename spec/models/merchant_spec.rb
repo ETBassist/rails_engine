0f24493e515cde02ca0df9a7a8628e78d065621a
Eugene Theriault <hybridbassist@gmail.com>
2020-12-13 11:11:05 -0800

    Create test for finding revenue for a merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -42,4 +42,26 @@ describe Merchant, type: :model do
       expect(Merchant.by_revenue(1)).to eq([@merchant2])
     end
   end
+
+  describe 'instance methods' do
+    before :each do
+      @merchant2 = create(:merchant)
+      invoice5 = create(:invoice, merchant: @merchant2, status: 'shipped')
+      invoice6 = create(:invoice, merchant: @merchant2, status: 'shipped')
+      invoice7 = create(:invoice, merchant: @merchant2, status: 'shipped')
+      invoice8 = create(:invoice, merchant: @merchant2, status: 'shipped')
+      create_list(:invoice_item, 10, invoice: invoice5, quantity: 40, unit_price: 11.0)
+      create_list(:invoice_item, 10, invoice: invoice6, quantity: 40, unit_price: 11.0)
+      create_list(:invoice_item, 10, invoice: invoice7, quantity: 40, unit_price: 11.0)
+      create_list(:invoice_item, 10, invoice: invoice8, quantity: 40, unit_price: 11.0)
+      create(:transaction, invoice: invoice5, result: 'success')
+      create(:transaction, invoice: invoice6, result: 'success')
+      create(:transaction, invoice: invoice7, result: 'success')
+      create(:transaction, invoice: invoice8, result: 'success')
+    end
+
+    it '#revenue' do
+      expect(@merchant2.revenue).to eq(17600.0)
+    end
+  end
 end
