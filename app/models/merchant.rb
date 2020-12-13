90c5a08fb59ce7da1b48b664d96caa63321bdd9e
Eugene Theriault <hybridbassist@gmail.com>
2020-12-13 10:07:59 -0800

    Create test for getting merchants by revenue
    Also edits invoice model to add has_many transactions relationship

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,11 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
 end
 
+  # Merchant.joins(invoices: [:invoice_items, :transactions])
+  # .select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+  # .group('merchants.id')
+  # .where("invoices.status = 'shipped' AND transactions.result = 'success'")
+
 #  create_table "merchants", force: :cascade do |t|
 #    t.string "name"
 #    t.datetime "created_at", null: false
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,4 +6,36 @@ describe Merchant, type: :model do
     it { should have_many :invoices }
     it { should have_many(:customers).through(:invoices) }
   end
+
+  describe 'class methods' do
+    it '::by_revenue' do
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant)
+      invoice1 = create(:invoice, merchant: merchant1, status: 'shipped')
+      invoice2 = create(:invoice, merchant: merchant1, status: 'shipped')
+      invoice3 = create(:invoice, merchant: merchant1, status: 'pending')
+      invoice4 = create(:invoice, merchant: merchant1, status: 'pending')
+      invoice5 = create(:invoice, merchant: merchant2, status: 'shipped')
+      invoice6 = create(:invoice, merchant: merchant2, status: 'shipped')
+      invoice7 = create(:invoice, merchant: merchant2, status: 'shipped')
+      invoice8 = create(:invoice, merchant: merchant2, status: 'shipped')
+      create_list(:invoice_item, 2, invoice: invoice1, quantity: 1, unit_price: 2.0)
+      create_list(:invoice_item, 4, invoice: invoice2, quantity: 3, unit_price: 4.0)
+      create_list(:invoice_item, 4, invoice: invoice3, quantity: 11, unit_price: 4.0)
+      create_list(:invoice_item, 4, invoice: invoice4, quantity: 46, unit_price: 4.0)
+      create_list(:invoice_item, 10, invoice: invoice5, quantity: 40, unit_price: 11.0)
+      create_list(:invoice_item, 10, invoice: invoice6, quantity: 40, unit_price: 11.0)
+      create_list(:invoice_item, 10, invoice: invoice7, quantity: 40, unit_price: 11.0)
+      create_list(:invoice_item, 10, invoice: invoice8, quantity: 40, unit_price: 11.0)
+      create(:transaction, invoice: invoice1, result: 'success')
+      create(:transaction, invoice: invoice2, result: 'success')
+      create(:transaction, invoice: invoice3, result: 'success')
+      create(:transaction, invoice: invoice4, result: 'failed')
+      create(:transaction, invoice: invoice5, result: 'success')
+      create(:transaction, invoice: invoice6, result: 'success')
+      create(:transaction, invoice: invoice7, result: 'success')
+      create(:transaction, invoice: invoice8, result: 'success')
+      expect(Merchant.by_revenue).to eq([merchant2, merchant1])
+    end
+  end
 end
