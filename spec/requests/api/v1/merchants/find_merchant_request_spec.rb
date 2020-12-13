9ac065dc92ad01320ac1bd8cd9d3760613877059
Eugene Theriault <hybridbassist@gmail.com>
2020-12-12 18:11:11 -0800

    Add test for finding multiple merchants by param

--- spec/requests/api/v1/merchants/find_merchant_request_spec.rb
+++ spec/requests/api/v1/merchants/find_merchant_request_spec.rb
@@ -4,6 +4,7 @@ describe 'Merchant GET request finds' do
   before :each do
     @merchant1 = Merchant.create!(name: 'Turing')
     @merchant2 = Merchant.create!(name: 'Haddersfield')
+    @merchant3 = Merchant.create!(name: 'Ring World')
   end
 
   it 'I can send a GET request to /api/v1/merchants/find?name=value' do
@@ -14,5 +15,19 @@ describe 'Merchant GET request finds' do
     expect(merchant_response[:data][:id]).to eq("#{@merchant1.id}")
     expect(merchant_response[:data][:type]).to eq('merchant')
     expect(merchant_response[:data][:attributes][:name]).to eq(@merchant1.name)
+  end
+
+  it 'I can search for multiple merchants with /api/v1/merchants/find_all?name=value' do
+    get '/api/v1/merchants/find_all?name=RING'
+
+    merchants_response = JSON.parase(response.body, symbolize_names: true)
+
+    expect(merchant_response[:data].size).to eq(2)
+
+    merchants_response[:data].each do |merchant|
+      expect(merchant[:id]).to be_a(String)
+      expect(merchant[:type]).to eq('merchant')
+      expect(merchant[:attributes]).to have_key(:name)
+    end
   end
 end
