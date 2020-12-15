e859672b2d701d851e170431b51f1b14e9a62199
Eugene Theriault <hybridbassist@gmail.com>
2020-12-15 13:05:40 -0800

    Removes commented out code

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,32 +1,4 @@
 class MerchantSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name
-
-#  def self.format_merchants(merchants)
-#    {
-#      data: merchants.map do |merchant|
-#        format_data_for(merchant)
-#      end
-#    }
-#  end
-#
-#  def self.format_merchant(merchant)
-#    { data: format_data_for(merchant) }
-#  end
-#
-#  module MerchantFormattable
-#    private
-#
-#    def format_data_for(merchant)
-#      {
-#        id: merchant.id.to_s,
-#        type: 'merchant',
-#        attributes: {
-#          name: merchant.name
-#        }
-#      }
-#    end
-#  end
-#
-#  extend MerchantFormattable
 end
