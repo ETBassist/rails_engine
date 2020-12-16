65c66cfb752ed96698e87a75a4a30405c7c90cb0
Eugene Theriault <hybridbassist@gmail.com>
2020-12-16 11:39:11 -0800

    Refactor successful invoice check into scope
    Also scopes created_between on invoice to check date range in which
    invoice was created instead of including that logic on Merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,36 +4,33 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
 
   def self.by_revenue(num_merchants = 10)
-    self.joins(invoices: [:invoice_items, :transactions])
+    self.joins(invoices: :invoice_items)
+      .merge(Invoice.successful)
       .select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
       .group(:id)
-      .where(invoices: {status: 'shipped'}) 
-      .where(transactions: {result: 'success'})
       .order('revenue DESC')
       .limit(num_merchants)
   end
 
   def self.revenue_between(date1, date2)
-    date_range = (date1.to_date.beginning_of_day..date2.to_date.end_of_day)
-    self.joins(invoices: [:invoice_items, :transactions])
-      .where("invoices.status = 'shipped' AND transactions.result = 'success'")
-      .where(invoices: {created_at: date_range})
+    self.joins(invoices: :invoice_items)
+      .merge(Invoice.successful)
+      .merge(Invoice.created_between(date1, date2))
       .sum('invoice_items.quantity * invoice_items.unit_price')
   end
 
   def self.by_items_sold(num_merchants = 10)
-    self.joins(invoices: [:invoice_items, :transactions])
+    self.joins(invoices: :invoice_items)
+      .merge(Invoice.successful)
       .select('merchants.*, SUM(invoice_items.quantity) AS items_sold')
       .group(:id)
-      .where(invoices: {status: 'shipped'}) 
-      .where(transactions: {result: 'success'})
       .order('items_sold DESC')
       .limit(num_merchants)
   end
 
   def revenue
-    invoices.joins(:invoice_items, :transactions)
-      .where("invoices.status = 'shipped' AND transactions.result = 'success'")
+    invoices.joins(:invoice_items)
+      .merge(Invoice.successful)
       .sum('invoice_items.quantity * invoice_items.unit_price')
   end
 end
