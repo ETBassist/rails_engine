608f0a02b43e118b7afe952accc12985e8ada385
Eugene Theriault <hybridbassist@gmail.com>
2020-12-16 14:09:27 -0800

    Switch to using RevenueFacade in controller actions

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RevenueController < ApplicationController
   def index
     revenue = Merchant.revenue_between(params[:start], params[:end])
-    render json: RevenueSerializer.new(Revenue.new(revenue))
+    render json: RevenueSerializer.new(RevenueFacade.format_revenue(revenue))
   end
 end
